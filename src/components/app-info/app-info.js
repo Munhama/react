7418b917ac3a221177bccaab848c27c3d2a3471a
Munhama <johnytynky@gmail.com>
2023-08-15 17:31:02 +0700

    Добавление компонентов и верстки проекта

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Учет сотрудников в компании N</h1>
+      <h2>Общее число сотрудников: XXX</h2>
+      <h2>Премию получат: XXX</h2>
+    </div>
+  );
+};
+
+export default AppInfo;
