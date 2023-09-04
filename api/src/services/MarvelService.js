bde7f5eea0ba04f8322860ceba684d3e4de38caf
Munhama <johnytynky@gmail.com>
2023-09-04 11:07:32 +0700

    Компонент со случайным персонажем

--- api/src/services/MarvelService.js
+++ api/src/services/MarvelService.js
@@ -1,6 +1,6 @@
 class MarvelService {
-    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=ec212461a0f8378de655fae7fc9c2366';
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=ec212461a0f8378de655fae7fc9c2366";
 
   getResource = async (url) => {
     let res = await fetch(url);
@@ -12,16 +12,30 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
+
+    return res.data.results.map(this._transformCharacter);
   };
 
-  getCharacter = (id) => {
-    return this.getResource(
+  getCharacter = async (id) => {
+    const res = await this.getResource(
       `${this._apiBase}characters/${id}?${this._apiKey}`
     );
+
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description ? char.description : 'Description is missing.',
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
